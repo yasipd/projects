56e4c2a36b240e04ec868ef2738b57fd63b5c369
“Yasaman <yasaman.pandand@sap.com>
2024-06-10 13:46:49 +0200

    inheritance
    learning inheritance and using the method of vehicle on the car without writing it again in the code

---
+++ learnJava1/Car.java
@@ -0,0 +1,9 @@
+package learnJava1;
+
+public class Car extends Vehicle {
+    int wheels;
+    void honk(){
+        System.out.println("Honk! Honk!");
+    }
+    
+}
---
+++ learnJava1/Main.java
@@ -0,0 +1,10 @@
+package learnJava1;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.go();
+        myCar.stop();
+        myCar.honk();
+    }
+}
