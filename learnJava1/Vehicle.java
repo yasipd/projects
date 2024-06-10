bf04d0a77f9cc9b6c95f23b1b5e0a4c105cd95cc
“Yasaman <yasaman.pandand@sap.com>
2024-06-10 13:35:22 +0200

    Create Vehicle.java
    learning inheritance

---
+++ learnJava1/Vehicle.java
@@ -0,0 +1,12 @@
+package learnJava1;
+
+public class Vehicle {
+    double speed;
+    void go(){
+        System.out.println("This vehicle is moving");
+    }
+    void stop(){
+        System.out.println("This vehicle is stopping");
+    }
+    
+}
