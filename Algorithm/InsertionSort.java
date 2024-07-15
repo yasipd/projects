c19240046bf297dc1fdd5d075da0d5d3681f55d1
“Yasaman <yasaman.pandand@sap.com>
2024-07-15 21:30:02 +0200

    insertionsort

---
+++ Algorithm/InsertionSort.java
@@ -0,0 +1,30 @@
+package Algoritm;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int[] array= {9,8,3,4,2,1,5,7,6};
+		insertionSort(array);
+		for(int i:array) {
+			System.out.print(i+" ");
+		}
+
+	}
+
+	private static void insertionSort(int[] array) {
+		for(int i=1;i<array.length;i++) {
+			int temp=array[i];
+			int j=i-1;
+				while(j>=0 && array[j]>temp) {
+					array[j+1]=array[j];
+					j--;
+				}array[j+1]=temp;
+			}
+				
+			}
+				
+		}
+		
+	
+
+
